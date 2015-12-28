b0503c70e7337333c50fcd75bf8ea9d5590d1ea7
Peter Camilleri <peter.c.camilleri@gmail.com>
2015-12-28 18:18:11 -0500

    Added options comments

--- lib/mini_readline/options.rb
+++ lib/mini_readline/options.rb
@@ -4,9 +4,10 @@
 module MiniReadline
 
   #The base options shared by all instances.
-  BASE_OPTIONS = {:window_width  => 79,
-                  :left_margin   => 10,
-                  :right_margin  => 10,
-                  :alt_prompt    => "<<< "}
+  BASE_OPTIONS = {:window_width  => 79,       #The width of the edit area.
+                  :left_margin   => 10,       #The left edit margin
+                  :right_margin  => 10,       #The right edit margin.
+                  :alt_prompt    => "<<< "    #The prompt when scrolled.
+                 }
 
 end
