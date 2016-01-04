3b329441131dfc1760591a2a3f52e7f48eb616b7
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-01-03 22:39:08 -0500

    Some files relocated.

--- lib/mini_readline/raw_term.rb
+++ lib/mini_readline/raw_term.rb
@@ -2,14 +2,13 @@
 
 #Select the type of platform in use.
 if (RUBY_PLATFORM =~ /\bcygwin\b/i) || (RUBY_PLATFORM !~ /mswin|mingw/)
-  require_relative 'other/raw_other'
+  require_relative 'raw_term/other/raw_other'
 else
-  require_relative 'windows/raw_windows'
+  require_relative 'raw_term/windows/raw_windows'
 end
 
 #* raw_term.rb - Platform determination for raw terminal access.
 module MiniReadline
   #Get an instance of a raw terminal controller object.
   BASE_OPTIONS[:term] = RawTerm.new
-
 end
