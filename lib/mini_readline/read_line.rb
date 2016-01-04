0c122ca0140115b3e8f1fce55ac16bb823aaa1eb
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-01-03 19:25:22 -0500

    Added a method to get the history buffer of a read line instance. Replaced clever code with clear code.

--- lib/mini_readline/read_line.rb
+++ lib/mini_readline/read_line.rb
@@ -14,11 +14,16 @@ module MiniReadline
 
     #Setup the instance of the mini line editor.
     #<br>Parameters:
-    #* history - An array of strings used to contain the history. Use an empty
+    #* buffer - An array of strings used to contain the history. Use an empty
     #  array to have a history buffer with no initial entries. Use the
     #  value nil (or false) to maintain no history at all.
-    def initialize(history=[])
-      @history = History.new(history)
+    def initialize(buffer=[])
+      @history = History.new(buffer)
+    end
+
+    #Get the history buffer of this read line instance.
+    def history
+      @history.history
     end
 
     #Read a line from the console with edit and history.
@@ -36,7 +41,8 @@ module MiniReadline
     #Initialize the read line process. This basically process the arguments
     #of the readline method.
     def initialize_readline(prompt, options = {})
-      @options, @working = MiniReadline::BASE_OPTIONS.merge(options), true
+      @options = MiniReadline::BASE_OPTIONS.merge(options)
+      @working = true
       (@term = @options[:term]).reset
 
       set_prompt(prompt)
