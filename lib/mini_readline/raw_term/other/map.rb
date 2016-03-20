c7f4d0cada01ec32f639c307d17eda2630364662
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-03-20 13:56:31 -0400

    Corrected the mapping of ctrl-b and ctrl-f under Linux/Mac.

--- lib/mini_readline/raw_term/other/map.rb
+++ lib/mini_readline/raw_term/other/map.rb
@@ -11,17 +11,17 @@ module MiniReadline
     #Left Arrows
     MAP["\e[D"]  = [:go_left]
     MAP["\e[D"]  = [:go_left]
+    MAP["\x02"]  = [:go_left]
 
     MAP["\e[1;5D"] = [:word_left]
-    MAP["\x02"]    = [:word_left]
     MAP["\eb"]     = [:word_left]
 
     #Right Arrows
     MAP["\e[C"]  = [:go_right]
     MAP["\eOC"]  = [:go_right]
+    MAP["\x06"]  = [:go_right]
 
     MAP["\e[1;5C"] = [:word_right]
-    MAP["\x06"]    = [:word_right]
     MAP["\ef"]     = [:word_right]
 
     #Up Arrows
--- lib/mini_readline/version.rb
+++ lib/mini_readline/version.rb
@@ -1,4 +1,4 @@
 module MiniReadline
   #The current version of the mini_readline gem.
-  VERSION = "0.4.0"
+  VERSION = "0.4.1"
 end
