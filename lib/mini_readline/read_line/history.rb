fc9322e0dcd720e5b0993658e8e31284bb89baa9
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-06-22 11:41:51 -0400

    Fixed a history bug.

--- lib/mini_readline/read_line/history.rb
+++ lib/mini_readline/read_line/history.rb
@@ -45,6 +45,7 @@ module MiniReadline
 
     #Append a string to the history buffer if enabled.
     def append_history(str)
+      str = str.chomp
       return              if @options[:no_blanks] && str.strip.empty?
       history.delete(str) if @options[:no_dups]
 
