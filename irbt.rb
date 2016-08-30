6c50ddfe1d5df5f3381c9d1af3e9a2602584a141
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-08-29 20:50:40 -0400

    Repaired the irbm utility.

--- irbt.rb
+++ irbt.rb
@@ -2,20 +2,55 @@
 # An IRB + mini_readline Test bed
 
 require 'irb'
-$force_alias_read_line_module = true
 
 puts "Starting an IRB console with mini_readline test bed."
 
 if ARGV[0] == 'old'
   ARGV.shift
+  puts "Using the standard readline facility."
+  no_patch = true
 elsif ARGV[0] == 'local'
   require_relative 'lib/mini_readline'
   puts "mini_readline loaded locally: #{MiniReadline::VERSION}"
-
+  no_patch = false
   ARGV.shift
 else
   require 'mini_readline'
   puts "mini_readline loaded from gem: #{MiniReadline::VERSION}"
+  no_patch = false
+end
+
+puts
+
+#Hack irb to use mini_readline.
+unless no_patch
+  module IRB
+    class ReadlineInputMethod < InputMethod
+
+      def initialize
+        super
+
+        @line_no = 0
+        @line = []
+        @eof = false
+
+        @stdin  = IO.open(STDIN.to_i,
+                          :external_encoding => IRB.conf[:LC_MESSAGES].encoding,
+                          :internal_encoding => "-")
+
+        @stdout = IO.open(STDOUT.to_i, 'w',
+                          :external_encoding => IRB.conf[:LC_MESSAGES].encoding,
+                          :internal_encoding => "-")
+      end
+
+      def gets
+        result = MiniReadline.readline(@prompt, true)
+        HISTORY.push(result) unless result.empty?
+        @line[@line_no += 1] = result
+      end
+
+    end
+  end
 end
 
 IRB.start
--- lib/mini_readline/version.rb
+++ lib/mini_readline/version.rb
@@ -1,4 +1,4 @@
 module MiniReadline
   #The current version of the mini_readline gem.
-  VERSION = "0.6.1"
+  VERSION = "0.6.2"
 end
