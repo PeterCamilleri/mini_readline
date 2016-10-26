479b17357743f3587cb5bc4f43ab195095ea00fe
Peter Camilleri <peter.c.camilleri@gmail.com>
2016-10-26 18:35:14 -0400

    Some further refactoring.

--- lib/mini_readline/raw_term/windows.rb
+++ lib/mini_readline/raw_term/windows.rb
@@ -29,6 +29,13 @@ module MiniReadline
     STD_OUTPUT_HANDLE = -11
 
     #Set up the Windows Raw Terminal.
+    #<br>Singleton Methods Added:
+    #* getch - Low-level get char.
+    #* kbhit - Low-level keyboard hit test.
+    #* beep - Low-level beep.
+    #* set_cursor_posn - Set the cursor position given coordinates (y*65536+x).
+    #* get_screen_info - Get info about the console.
+    #* get_handle - Get the internal handle associated with a file index.
     def initialize
       getch_proc = Win32API.new("msvcrt", "_getch", [], 'I')
       define_singleton_method(:getch) { getch_proc.call.chr }
@@ -43,16 +50,16 @@ module MiniReadline
                                           "SetConsoleCursorPosition",
                                           ['L','L'], 'L')
 
-      define_singleton_method(:set_cursor_posn) do |handle, position|
-        set_cursor_posn_proc.call(handle, position)
+      define_singleton_method(:set_cursor_posn) do |position|
+        set_cursor_posn_proc.call(@_out_handle, position)
       end
 
       get_screen_info_proc = Win32API.new("kernel32",
                                           "GetConsoleScreenBufferInfo",
                                           ['L','P'], 'L')
 
-      define_singleton_method(:get_screen_info) do |handle, buffer|
-        get_screen_info_proc.call(handle, buffer)
+      define_singleton_method(:get_screen_info) do |buffer|
+        get_screen_info_proc.call(@_out_handle, buffer)
       end
 
       get_handle_proc = Win32API.new("kernel32", "GetStdHandle", ['L'], 'L')
