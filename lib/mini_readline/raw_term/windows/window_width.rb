6eb4551beda8506606ab9fa0f370712f4404e266
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-10-10 20:48:05 -0400

    Refined console info.

--- lib/mini_readline/raw_term/windows/window_width.rb
+++ lib/mini_readline/raw_term/windows/window_width.rb
@@ -17,9 +17,10 @@ module MiniReadline
       raw_buffer = 0.chr * 24
       get_screen_info(raw_buffer)
 
-      left, top, right, bottom = raw_buffer[10,8].unpack('SSSS')
+      width = (raw_buffer[0,2].unpack('S'))[0]
+      _left, top, _right, bottom = raw_buffer[10,8].unpack('SSSS')
 
-      [right - left + 1, bottom - top + 1]
+      [width, bottom - top + 1]
     end
 
   end
