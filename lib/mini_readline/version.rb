7261f542b577cff2d27eb917c90112efe50285b1
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-12-31 12:05:09 -0500

    New for version 0.9.1

--- lib/mini_readline/version.rb
+++ lib/mini_readline/version.rb
@@ -3,8 +3,8 @@
 # Version info for the gem.
 module MiniReadline
   #The current version of the mini_readline gem.
-  VERSION = "0.9.0".freeze
+  VERSION = "0.9.1".freeze
 
   # A brief description.
-  DESCRIPTION = "Get console input with edit, history, and auto-complete.".freeze
+  DESCRIPTION = "mini_readline: Get console input with edit, history, and auto-complete.".freeze
 end
